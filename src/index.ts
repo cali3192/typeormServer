0e82a3d4ac3b1f586bb491e76a550ba22e60d002
Charmaine Tabilas <charmainetabilas@Charmaines-MacBook-Air-3.local>
2019-12-26 17:07:43 -0500

    playing with request headers

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import 'reflect-metadata';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import { createConnection, Connection } from "typeorm";
+
+import "reflect-metadata";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 // import bodyParser from 'body-parser';
 // import expressWinston from 'express-winston';
-
-import { createConnection } from 'typeorm';
 // import { User } from './entities/User';
 
-import routes from './routes';
+import routes from "./routes";
 
 createConnection()
-  .then(async (connection) => {
+  .then(async connection => {
+    console.log(`successful connection`);
     const app = express();
+    console.log(`express app up`);
 
     app.use(bodyParser.json());
-    app.use('/', routes);
-
-    app.get('/', (req, res) => {
-      res.send('will this actually work');
-    });
+    app.use("/", routes);
 
     const PORT = 4000;
 
@@ -26,4 +24,4 @@ createConnection()
       console.log(`listneing on ${PORT}`);
     });
   })
-  .catch((error) => console.log(`error in the server file \n\n`, error));
+  .catch(error => console.log(`error in the server file \n\n`, error));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
-import { Router, Request, Response, Next } from 'express';
-import SayHi from '../controllers/';
+import { Router, Request, Response, Next } from "express";
+// import SayHi from "../controllers/";
 
 const routes = Router();
 
-routes.get('/route', (req: Request, res: Response) => {
-  res.send('I am the route!');
+routes.get("/route", (req: Request, res: Response) => {
+  res.send("I am the route!");
 });
 
-routes.get('/hi', SayHi);
+routes.get("/hi", (request: Request, response: Response) => {
+  console.log(`hi route`);
+  console.log(`request headers: `, request.headers);
+  response.send(`Hello, World`);
+});
 
 export default routes;
